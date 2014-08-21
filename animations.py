fd9d5053207dabef3f913e1525194d4c90003259
HallaSurvivor <hallasurvivor@gmail.com>
2014-08-21 02:37:51 -0400

    wrote function to call gifs for me

--- animations.py
+++ animations.py
@@ -1,36 +1,24 @@
 import pyganim
 import pygame
 from PIL import Image, ImageSequence
-# bulbasaur_front = pyganim.PygAnimation([
-#    ('sprites/bulbasaur/front0.gif', 0.1),
-#    ('sprites/bulbasaur/front1.gif', 0.1),
-#    ('sprites/bulbasaur/front2.gif', 0.1),
-#    ('sprites/bulbasaur/front3.gif', 0.1),
-#    ('sprites/bulbasaur/front4.gif', 0.1),
-#    ('sprites/bulbasaur/front5.gif', 0.1),
-#    ('sprites/bulbasaur/front6.gif', 0.1),
-#    ('sprites/bulbasaur/front7.gif', 0.1),
-#    ('sprites/bulbasaur/front8.gif', 0.1),
-#    ('sprites/bulbasaur/front9.gif', 0.1),
-#    ('sprites/bulbasaur/front10.gif', 0.1),
-# ])
 
-bulbasaur_front_gif = Image.open('sprites/bulbasaur.gif')
-sprite_list = []
+def import_gif(gif):
+    PIL_gif = Image.open(gif)
+    sprite_list = []
 
-for frame in ImageSequence.Iterator(bulbasaur_front_gif):
-    frame = frame.convert("RGBA")
-    mode = frame.mode
-    size = frame.size
-    data = frame.tobytes()
+    for frame in ImageSequence.Iterator(PIL_gif):
+        frame = frame.convert("RGBA")
+        mode = frame.mode
+        size = frame.size
+        data = frame.tobytes()
 
-    print(mode, size)
+        sprite = pygame.image.frombuffer(data, size, mode)
+        sprite_list.append((sprite, 0.1))
 
-    sprite = pygame.image.frombuffer(data, size, mode)
-    test_image = sprite
-    sprite_list.append((sprite, 0.1))
+    return pyganim.PygAnimation(sprite_list)
 
-bulbasaur_front = pyganim.PygAnimation(sprite_list)
+
+bulbasaur_front = import_gif('sprites/bulbasaur.gif')
 
 bulbasaur_back = pyganim.PygAnimation([
     ('sprites/bulbasaur/back0.gif', 0.1),
